2e9698710de79f227c1351a931a57f5502371515
ibold-dev <85069769+ibold-dev@users.noreply.github.com>
2024-11-20 08:58:43 -0800

    Update#22

--- tg.js
+++ tg.js
@@ -1,8 +1,54 @@
 function getTelegramUserData() {
   const telegram = window.Telegram.WebApp;
+  // Retrieve user information
   const user = telegram.initDataUnsafe.user;
+  const initData = telegram.initData;
 
-  if (user) {
+  const payload = {
+    user: user
+      ? {
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          username: user.username,
+          photo_url: user.photo_url,
+        }
+      : null,
+    initData: initData,
+  };
+  sendInitDataToServer(payload);
+}
+
+function sendInitDataToServer(data) {
+  const serverUrl = "https://runnercatserver.onrender.com/v1/web/validate"; // Replace with your actual server URL
+
+  fetch(serverUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        return null;
+      }
+      return response.json();
+    })
+    .then((responseData) => {
+      console.log("Data from server:", responseData);
+      return JSON.stringify(responseData.data);
+    })
+    .catch((error) => {
+      console.error("Error sending data to server:", error);
+    });
+}
+
+// DEPRECATED
+
+/**
+ * 
+ *   if (user) {
     return JSON.stringify({
       id: user.id,
       first_name: user.first_name,
@@ -13,4 +59,4 @@ function getTelegramUserData() {
   } else {
     return JSON.stringify({ error: "No user information available." });
   }
-}
+ */
