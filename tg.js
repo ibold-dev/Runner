6e10d11fb8bf1b020164ba2a5b3e105c8bb6fdfb
ibold-dev <85069769+ibold-dev@users.noreply.github.com>
2024-11-21 19:16:56 -0800

    Update tg.js

--- tg.js
+++ tg.js
@@ -1,7 +1,7 @@
 function getTelegramUserData() {
   const initData = window.Telegram.WebApp.initData;
   if (initData) {
-    return initData;
+    return JSON.stringify({ data: initData });
   } else {
     return null;
   }
